81a89c3008893d96c4f3391481cb5abc220c0083
Salvador Aceves <xalakox@gmail.com>
2012-08-04 13:48:14 -0500

    a better way..

--- json2csv.rb
+++ json2csv.rb
@@ -1,66 +1,48 @@
 require 'rubygems'
 require 'json'
-
 def getheaders(name,value,parent)
-	name = name.to_s
-	parent = parent.to_s
 	ret = ""
-	if value.class.to_s == "Hash"
-		value.each do |k, v| 
-			family = name
-			family = parent + "['" + name + "']" if parent!=""
-			ret = ret + getheaders(k,v,family)
-		end
-	elsif value.class.to_s == "Array"
-		value.each_index do |i|
-			family = name
-			family = parent + name if parent!=""
-			ret = ret + getheaders("["+i.to_s+"]",value[i],family)
-		end
-		
-	elsif value.class.to_s == "Fixnum"
-		ret = ret + parent + "["+ name + "],"
-	elsif parent!=""
-		ret = ret +  parent + "['" + name + "'],"
+	if name.class.to_s == "Fixnum"
+		name = "[" + name.to_s + "]"
 	else
-		ret = ret + "['"+name + "'],"
+		name = "['" + name.to_s + "']" if name !=""
+	end
+	if parent!=""
+		family = parent + name
+	else
+		family = name.to_s
+	end
+	case value.class.to_s
+		when "Hash"
+			value.each do |k, v| 
+				ret = ret + getheaders(k,v,family)
+			end
+		when "Array"
+			value.each_index do |i|
+				ret = ret + getheaders(i,value[i],family)
+			end
+		else
+			ret = ret + family + ","
 	end
 	return ret
 end
-
 def tocsv(row,heads)
 	ret = ""
 	heads.each do |h|
 		exec = "row"+h
+		val = ""
 		begin
-			ret += eval(exec)
+			val = eval(exec)
+			if val.class.to_s == "String"
+				val = '"' + val.sub('"','""') + '"'
+			end
 		rescue Exception => exc
-			ret += ""
+			
 		end
-		ret +=","
+		ret += val.to_s+","
 	end
 	return ret
 end
-
-				# ret = '';
-				# for (var h in heads){
-				# 	prop = heads[h].toString().replace(/\\[/gi,\"'][\");
-				# 	prop = \"['\"+prop.replace(/\\./gi,\"']['\");
-				# 	if (prop.charAt(prop.length-1)!=']') prop = prop+\"']\";
-				# 	cmd = 'therow'+prop;
-				# 	try{
-				# 		valor = eval(cmd);
-				# 		valor = valor.toString().replace(/,/gi,' ');
-				# 		valor = valor.toString().replace(/(\\r\\n|\\n|\\r)/gm,'');
-				# 	}
-				# 	catch(er){
-				# 		valor = '';
-				# 	}
-				# 	ret = ret + valor + ',';
-				# }
-				# return ret;
-
-
 if ARGV[0] && ARGV[0] != "" && ARGV[1] && ARGV[1] != ""
 	json = ""
 	file = File.open(ARGV[0], 'r')
@@ -74,7 +56,6 @@ if ARGV[0] && ARGV[0] != "" && ARGV[1] && ARGV[1] != ""
 	puts headers.inspect
 	json.each do |data|
 		puts tocsv(data,headers)
-		exit 1
 	end
 
 else
